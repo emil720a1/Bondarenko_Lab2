f0ad33d00264a8629cb6710ffebaeecc38074477
emil720a1 <emil720apro@gmail.com>
2024-10-02 23:00:56 +0300

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 using namespace std;
-int main (){
-   int arr[4]={1,2,3,1};
-   bool polindrom = true;
-   for(int i=0;i<4/2;i++){
-    if (arr[i]!=arr[4-1-i]){
-        polindrom = false;
-        break;
-    }
-   }
-   if(!polindrom){
-    cout<<"arr not palindrom";
 
-   } else{cout<<"arrpolindrom";}
-   
-   
+int main() {
+    int number, original_num, reversed_num = 0;
+
+    cout << "Enter number: ";
+    cin >> number;
+
+    original_num = number;
+
+    while (number > 0) {
+        int remainder = number % 10;
+        reversed_num = reversed_num * 10 + remainder;
+        number /= 10;
+    }
+
+    if (original_num == reversed_num) {
+        cout << original_num << "Palindrom" << endl;
+    }
+    else {
+        cout << original_num << " is not palindrom" << endl;
+    }
+
     return 0;
 }
